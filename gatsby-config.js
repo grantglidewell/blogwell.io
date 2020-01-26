d9ac9c734262be60b1e8f4eb81da2e8ba765e1ad
Grant Glidewell <ggcadc@gmail.com>
2020-01-26 09:07:51 -0800

    added feed.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,61 @@
 module.exports = {
   siteMetadata: {
     title: `Grant Glidewell | Blog`,
+    siteUrl: `https://blogwell.io`,
     description: `Grant Glidewell is a JavaScript engineer living in San Diego, CA. He loves science fiction, dogs, and coffee.`,
     author: `@grantglidewell`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-feed-generator`,
+      options: {
+        json: true,
+        siteQuery: `
+    {
+      site {
+        siteMetadata {
+          title
+          description
+          siteUrl
+          author
+        }
+      }
+    }
+  `,
+        feeds: [
+          {
+            name: `feed`,
+            query: `
+      {allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+        nodes {
+          frontmatter {
+            author
+            description
+            title
+            date(formatString: "MM-DD-YYYY")
+          }
+          fields {
+            slug
+          }
+        }
+      }}
+      `,
+            normalize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return {
+                  title: node.frontmatter.title,
+                  date: node.frontmatter.date,
+                  description: node.frontmatter.description,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  html: node.html,
+                }
+              })
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
